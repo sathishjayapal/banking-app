75b228bdc41f0d726047cc9fdb169fc8f58493a8
Sathish Jayapal <sjayapal@skminfotech.com>
2023-12-19 17:15:41 -0600

    Initial commit

---
+++ accounts/src/test/java/me/sathish/bank/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.sathish.bank;
+
+import me.sathish.bank.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
