4f5a93af7e2021bc000ae129efa0d137521e6a2e
Sathish Jayapal <sjayapal@skminfotech.com>
2023-12-27 15:47:02 -0600

    Added MC config files

--- projectsconfigserver/src/main/java/me/sathish/projectsconfigserver/ProjectsConfigServerApplication.java
+++ projectsconfigserver/src/main/java/me/sathish/projectsconfigserver/ProjectsConfigServerApplication.java
@@ -3,8 +3,12 @@ package me.sathish.projectsconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-   (String[] args) {
-		SpringApplication.run(ProjectsconfigserverApplication.class, args);
-	}
 
+@SpringBootApplication
+@EnableConfigServer
+public class ProjectsConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsConfigServerApplication.class, args);
+    }
 }
